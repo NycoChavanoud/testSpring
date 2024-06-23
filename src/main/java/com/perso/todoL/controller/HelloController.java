0fede37da69327fb22aba4419477d7efc3083279
NycoChavanoud <nicolas.chavanoud@gmail.com>
2024-06-23 22:37:36 +0200

    first commit

---
+++ src/main/java/com/perso/todoL/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.perso.todoL.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/main/java/com/perso/todoL/controller/TodoController.java
@@ -0,0 +1,42 @@
+package com.perso.todoL.controller;
+
+import com.perso.todoL.model.Todo;
+import com.perso.todoL.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("//")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Todo> getTodoById(@PathVariable Long id) {
+        return todoService.getTodoById(id);
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoService.createTodo(todo);
+    }
+
+    @PutMapping("/{id}")
+    public Todo updateTodo(@PathVariable Long id, @RequestBody Todo todoDetails) {
+        return todoService.updateTodo(id, todoDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id) {
+        todoService.deleteTodo(id);
+    }
+}
